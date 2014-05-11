ebe408a4fed8c9ea7bbb32199baf3bd4656cfb49
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-05-11 18:55:39 -0300

    Add name to courses

--- db/migrate/20140511214224_create_courses.rb
+++ db/migrate/20140511214224_create_courses.rb
@@ -1,6 +1,7 @@
 class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
+      t.string :name
       t.float :first_unity
       t.float :second_unity
       t.float :third_unity
