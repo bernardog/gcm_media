1929a0fdd6684c778a9471efbf903180f345ce3e
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-05-11 22:49:02 -0300

    Add some tests to course model

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,22 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "status should be 'aprovado' if average >= 5" do
+    @course = courses(:approved)
+    @course.save
+    assert_equal @course.status, 'aprovado'
+  end
+
+  test 'status should be retake if 3 <= average < 5' do
+    @course = courses(:retake)
+    @course.save
+    assert_equal @course.status, 'em recuperação'
+  end
+
+  test 'status should be retake if average < 3' do
+    @course = courses(:failed)
+    @course.save
+    assert_equal @course.status, 'reprovado'
+  end
 end
